f488553694ab19a1cb4a9c92008bccbbc7014bc9
Keshab Chandra Nayak <kesavnayak@gmail.com>
2023-04-23 12:16:24 +0530

    Angular

---
+++ frontend/src/app/employee-list/employee-list.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { Employee } from '../employee';
+import { EmployeeService } from '../employee.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-employee-list',
+  templateUrl: './employee-list.component.html',
+  styleUrls: ['./employee-list.component.scss']
+})
+export class EmployeeListComponent {
+
+  employees?: Employee[];
+
+  constructor(private employeeService: EmployeeService,
+    private router: Router){
+    }
+
+    ngOnInit(): void {
+      this.getEmployees();
+    }
+
+    private getEmployees(){
+      this.employeeService.getEmployeesList().subscribe(data => {
+        debugger;
+        this.employees = data;
+      });
+    }
+
+    employeeDetails(id: number){
+      this.router.navigate(['employee-details', id]);
+    }
+  
+    updateEmployee(id: number){
+      this.router.navigate(['update-employee', id]);
+    }
+  
+    deleteEmployee(id: number){
+      this.employeeService.deleteEmployee(id).subscribe( data => {
+        console.log(data);
+        this.getEmployees();
+      })
+    }
+}
---
+++ frontend/src/app/employee.ts
@@ -0,0 +1,7 @@
+export class Employee {
+    employeeId: number =0;
+    name?: string;
+    gender?: string;
+    department?: string;
+    city?: string;
+}
